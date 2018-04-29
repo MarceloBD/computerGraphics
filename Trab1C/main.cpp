0897a0a142015e6f267288854ecf1f0e3e100ab2
Marcelo <marcelodianib@gmail.com>
2018-04-29 14:32:45 -0300

    main working

--- Trab1C/main.cpp
+++ Trab1C/main.cpp
@@ -1,6 +1,11 @@
 #include "main.hpp"
 
-Point::Point (GLint x, GLint y){
+const GLint WINDOW_WIDTH = 800;
+const GLint WINDOW_HEIGHT = 600;
+const GLchar TITLE[30] = "This is a black spider";
+GLint screen_width, screen_height;
+GLint positionx, positiony;
+/*Point::Point (GLint x, GLint y){
 	this->x = x;
 	this->y = y;
 }
@@ -33,4 +38,73 @@ Spider::Spider(Point initialPos){
 		positions[1] = new Point(x + rx*cos(PI/6));
 		positions[2] = new Point(x + rx*cos(PI/4));
 	}TODO: finish legs positions */
+//}
+
+void mouseListener(GLint button, GLint action, GLint x, GLint y) {
+  switch (button) {
+    case GLUT_LEFT_BUTTON: {
+     	std::cout<<"Esquerda";
+      //	Model *test = static_cast<Model*>(model);
+      //	test->moveSpider(x, y);
+  //    	init();
+
+
+      break;
+    }
+    case GLUT_MIDDLE_BUTTON: {
+     	std::cout<<"Meio";
+      break;
+    }
+    case GLUT_RIGHT_BUTTON: {
+     	std::cout<<"Direita";
+      break;
+    }
+	}
+  	if (action == GLUT_DOWN) {
+    	std::cout<<" preciona";
+    }
+    else {
+    	std::cout<<"libera";
+
+    }
+    std::cout<<" em (x:"<<x<<", y:"<<y<<")\n";
+}
+
+void init() {
+  glClearColor(1.0, 1.0, 1.0, 1.0);     
+  glMatrixMode(GL_PROJECTION);
+  glClear(GL_COLOR_BUFFER_BIT);
+  glLoadIdentity();
+  glOrtho(0.0f, WINDOW_WIDTH, WINDOW_HEIGHT, 0.0f, 0.0f, 1.0f);
+  glFlush();
+}
+
+void view(int argc, char **argv) {
+
+	glutInit(&argc, argv);  
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);   
+
+  screen_width  = glutGet(GLUT_SCREEN_WIDTH),
+  screen_height = glutGet(GLUT_SCREEN_HEIGHT);  
+  
+  positionx = (screen_width-WINDOW_WIDTH)/2;
+  positiony = (screen_height-WINDOW_HEIGHT)/2;
+
+  glutInitWindowPosition(positionx, positiony);  //Utilizada para definir a posição inicial da janela, sendo que os parâmetros representam a posição do canto superior esquerdo
+ 	glutInitWindowSize(WINDOW_WIDTH, WINDOW_WIDTH);                                                 // Define a largura e altura da janela
+  glutCreateWindow(TITLE);                             // Cria a janela, sendo que o parâmetro será o título dela
+  
+  init();                                                  
+  
+}
+
+
+
+
+int main(int argc, char **argv) {
+	view(argc, argv);
+	glutMouseFunc(mouseListener);
+	
+	glutMainLoop();
+	return 1; 
 }
